62d11c9371e83493b656d3a5851407dd392fed02
auswitch <90751246+auswitch@users.noreply.github.com>
2023-11-14 14:42:23 +0700

    docs selected

--- src/modules/callback/pages/studentHome.tsx
+++ src/modules/callback/pages/studentHome.tsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import Genpdf from "./genPdf";
 
 function StudentHome() {
+  const [selectedDoc, setSelectedDoc] = useState<string | null>(null);
+  const docOptions = [
+    "หนังสือรับรองความประพฤติ",
+    "หนังสือรับรองการเป็นนักศึกษา",
+    "หนังสือขอลงทะเบียนเรียนภาคฤดูร้อน",
+    "ใบคำขอทั่วไป"
+  ];
+
   return (
     <div className="body">
-      
-      <p className=" mt-56">this is StudentHome</p>
-     <Genpdf />
+      <p className=" mt-56">ขอใบคำขอ</p>
+      <select onChange={(e) => setSelectedDoc(e.target.value)} style={dropdown}>
+        <option value="">Select</option>
+        {docOptions.map((prenames, index) => (
+          <option key={index} value={prenames}>
+            {prenames}
+          </option>
+        ))}
+      </select>
+      {selectedDoc !== null && selectedDoc !== "" && <Genpdf docs={selectedDoc} />}
     </div>
   );
 }
 
 export default StudentHome;
+
+const dropdown = {
+  borderRadius: "5px",
+  border: "1px solid var(--white-gray-6-gray, #E1E1E1)",
+  background: "#FFF",
+  color: "#838383"
+};
